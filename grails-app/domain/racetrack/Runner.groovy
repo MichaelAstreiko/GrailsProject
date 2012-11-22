bf780e577c5ebd79fd8391588bb951d1b71dc6c8
Vladimir Havenchyk <vladimir.havenchyk@realizeideas.net>
2012-11-22 12:48:42 +0200

    add runner for storage data of runner

---
+++ grails-app/domain/racetrack/Runner.groovy
@@ -0,0 +1,32 @@
+package racetrack
+
+class Runner {
+    String firstName
+    String lastName
+    String gender
+    String address
+    String city
+    String state
+    String zipcode
+    String email
+    Date dateOfBirth
+
+
+    static hasMany = [registrations: Registration]
+    static constraints = {
+        firstName(blank: false)
+        lastName(blank: false)
+        dateOfBirth()
+        gender(inList: ["M", "F"])
+        address()
+        city()
+        state()
+        zipcode()
+        email(email: true)
+    }
+
+    @Override
+    String toString() {
+        return "${lastName}, ${firstName} (${email})"
+    }
+}
