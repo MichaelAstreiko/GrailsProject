f0a9baec51ace21307f71128e5a6624e628e553d
Vladimir Havenchyk <vladimir.havenchyk@realizeideas.net>
2012-11-22 15:45:00 +0200

    comment simply adding of data during init

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,45 +5,45 @@ import racetrack.Registration
 class BootStrap {
 
     def init = { servletContext ->
-        def jane = new Runner(
-                firstName: "Jane",
-                lastName: "Doe",
-                dateOfBirth: (new Date() - 365 * 30),
-                gender: "F",
-                address: "123 Main Street",
-                city: "NY",
-                state: "NY",
-                zipcode: "2201023",
-                email: "jane@gmail.com"
-        )
-        jane.save()
-        if (jane.hasErrors()) {
-            println jane.errors
-        }
-
-        def raceOfMinsk = new Race(
-                city: "Minsk",
-                state: "Minskaya vobl",
-                cost: 100,
-                distance: 100400,
-                maxRunners: 100499,
-                name: "Race of Minsk",
-                startDate: (new Date() + 100),
-        )
-        raceOfMinsk.save()
-        if (raceOfMinsk.hasErrors()) {
-            println raceOfMinsk.errors
-        }
-
-        def registration = new Registration(
-                race: raceOfMinsk,
-                paid: true,
-                runner: jane
-        )
-        registration.save()
-        if (registration.hasErrors()) {
-            println registration.errors
-        }
+//        def jane = new Runner(
+//                firstName: "Jane",
+//                lastName: "Doe",
+//                dateOfBirth: (new Date() - 365 * 30),
+//                gender: "F",
+//                address: "123 Main Street",
+//                city: "NY",
+//                state: "NY",
+//                zipcode: "2201023",
+//                email: "jane@gmail.com"
+//        )
+//        jane.save()
+//        if (jane.hasErrors()) {
+//            println jane.errors
+//        }
+//
+//        def raceOfMinsk = new Race(
+//                city: "Minsk",
+//                state: "Minskaya vobl",
+//                cost: 100,
+//                distance: 100400,
+//                maxRunners: 100499,
+//                name: "Race of Minsk",
+//                startDate: (new Date() + 100),
+//        )
+//        raceOfMinsk.save()
+//        if (raceOfMinsk.hasErrors()) {
+//            println raceOfMinsk.errors
+//        }
+//
+//        def registration = new Registration(
+//                race: raceOfMinsk,
+//                paid: true,
+//                runner: jane
+//        )
+//        registration.save()
+//        if (registration.hasErrors()) {
+//            println registration.errors
+//        }
     }
     def destroy = {
     }
