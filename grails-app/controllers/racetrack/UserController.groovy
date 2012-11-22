d20af55ce9779ff6af3f7842b035f8b59b6ff000
Vladimir Havenchyk <vladimir.havenchyk@realizeideas.net>
2012-11-22 16:14:44 +0200

    add user controller

---
+++ grails-app/controllers/racetrack/UserController.groovy
@@ -0,0 +1,26 @@
+package racetrack
+
+class UserController {
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+
+    def index = {
+        redirect(action: "list", params: params)
+    }
+
+    def list = {
+        params.max = Math.min(params.max ? params.int('max') : 10, 100)[userInstance: User.list(params), userInstanceTotal: User.count()]
+    }
+
+    def create = {
+        def userInstance = new User()
+        userInstance.properties = params
+        return [userInstance: userInstance]
+    }
+
+
+    def save = {}
+    def show = {}
+    def edit = {}
+    def update = {}
+    def delete = {}
+}
